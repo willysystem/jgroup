f35ce0acca95f9b22ae85f021c00e992e48423c4
willysystem <willysystems@gmail.com>
2017-01-18 11:44:01 -0400

    no decorated panel

--- creditos-web/src/com/jgroup/creditos/client/Creditos_web.java
+++ creditos-web/src/com/jgroup/creditos/client/Creditos_web.java
@@ -3,8 +3,11 @@ package com.jgroup.creditos.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.DecoratedTabPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabLayoutPanel;
+import com.jgroup.creditos.view.CotizacionVista;
+import com.jgroup.creditos.view.CreditosVista;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -13,7 +16,9 @@ public class Creditos_web implements EntryPoint {
 	
 	public void onModuleLoad() {
 		
-	    TabLayoutPanel tabPanel = new TabLayoutPanel(2.5, Unit.EM);
+		
+		
+		DecoratedTabPanel tabPanel = new DecoratedTabPanel(/*2.5, Unit.EM*/);
 	    
 	    tabPanel.setPixelSize(Window.getClientWidth()-30, Window.getClientHeight()+100);
 	    //tabPanel.getElement().getStyle().setMarginBottom(10.0, Unit.PX);
